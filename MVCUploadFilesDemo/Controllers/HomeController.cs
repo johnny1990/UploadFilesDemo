d9c9cd442daf7c29fcddae6550a34e9569c512fc
Man Ioan <man_ioan@live.com>
2019-12-26 18:34:37 +0200

    Add Upload component

--- MVCUploadFilesDemo/Controllers/HomeController.cs
+++ MVCUploadFilesDemo/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +9,32 @@ namespace MVCUploadFilesDemo.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult UploadFiles()
+        {
+            string FileName = "";
+            HttpFileCollectionBase files = Request.Files;
+            for (int i = 0; i < files.Count; i++)
+            {
+                HttpPostedFileBase file = files[i];
+                string fname;
+
+                
+                    string[] testfiles = file.FileName.Split(new char[] { '\\' });
+                    fname = testfiles[testfiles.Length - 1];
+                
+ 
+                fname = Path.Combine(Server.MapPath("~/Uploads/"), fname);
+                file.SaveAs(fname);
+            }
+            return Json(FileName, JsonRequestBehavior.AllowGet);
+        }       
 
         public ActionResult About()
         {
---
+++ MVCUploadFilesDemo/Scripts/Upload.js
@@ -0,0 +1,29 @@
+﻿$('#Upload').click(function () {
+
+    var fileUpload = $("#Files").get(0);
+    var files = fileUpload.files;
+   
+    var fileData = new FormData();
+
+    for (var i = 0; i < files.length; i++) {
+        fileData.append(files[i].name, files[i]);
+    }
+    $.ajax({
+        url: '/Home/UploadFiles',
+        type: "POST",
+        contentType: false, 
+        processData: false, 
+        data: fileData,
+        async: false,
+        success: function (result) {
+            if (result != "") {
+                $('#BrowseFile').find("*").prop("disabled", true);
+
+            }
+        },
+        error: function (err) {
+            alert(err.statusText);
+        }
+    });
+
+}); 
